1d2e7dc42e31697c409e2de58951d5b300c7a106
曾慧兰 <234808961@qq.com>
2025-11-05 19:36:29 +0800

    图书管理员的注册单个页面

--- src/views/MasterRegister-page.vue
+++ src/views/MasterRegister-page.vue
@@ -1,29 +1,26 @@
 <template>
-<div class="masterregister-page">
+  <div class="masterregister-page">
     <MasterLogin v-if="currentForm === 'login'" />
-    <MasterRegister
-    v-else
-    :switch-to-login="switchToLogin"
-    :on-submit="handleRegister"
-    />
-</div>
+    <MasterRegister v-else :switch-to-login="switchToLogin" :on-submit="handleRegister" />
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import MasterLogin from '@/components/MasterLogin.vue';
-import MasterRegister from '@/components/MasterRegister.vue';
+import { ref } from 'vue'
+import MasterLogin from '@/views/Auth-page(masterLogin).vue'
+import MasterRegister from '@/components/MasterRegister.vue'
 
-const currentForm = ref('register'); // 默认注册页面
+const currentForm = ref('register') // 默认注册页面
 
 const handleRegister = (formData) => {
-    console.log('注册数据:', formData);
-};
+  console.log('注册数据:', formData)
+}
+
 
 // 从注册切换到登录
 const switchToLogin = () => {
-    currentForm.value = 'login';
-};
+  currentForm.value = 'login'
+}
 </script>
 
 <style scoped>
