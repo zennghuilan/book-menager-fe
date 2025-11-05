e1b526d6faf518b10e5716719df4b5aaf1c5b8fe
曾慧兰 <234808961@qq.com>
2025-11-05 14:31:30 +0800

    读者的重置密码

---
+++ src/views/ReaderResetpassword-page.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="readerresetpassword-page">
+    <ReaderLogin v-if="currentForm === 'login'" />
+    <Resetpassword
+    v-else
+    :switch-to-login="switchToLogin"
+    :on-submit="handleResetpassword"
+    />
+</div>
+</template>
+//在在这里好像就是只有登录的接口不一样 能不能实现合并捏
+<script setup>
+import { ref } from 'vue';
+
+
+const currentForm = ref('reset'); //初始量
+
+const handleResetpassword = (formData) => {
+    console.log('重置数据:', formData);
+};
+
+// 从重置密码切换到登录
+const switchToLogin = () => {
+    currentForm.value = 'login';
+};
+</script>
+
+<style scoped>
+.masterresetpassword-page {
+  display: flex;
+  width: 75%;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
