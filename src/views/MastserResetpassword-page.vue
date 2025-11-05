59e7e9f32d0dda790d8867056b0307b671bd0440
曾慧兰 <234808961@qq.com>
2025-11-05 13:58:53 +0800

    完成图书管理员注册和返回登录的页面

---
+++ src/views/MastserResetpassword-page.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="masterresetpassword-page">
+    <MasterLogin  Login v-if="currentForm === 'login'" />
+    <Resetpassword
+      v-else
+      :switch-to-login="switchToLogin"
+      :on-submit="handleResetpassword"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Resetpassword from '@/components/ResetPassword.vue';
+import MasterLogin from '@/components/MasterLogin.vue';
+
+const currentForm = ref('login');
+
+const handleResetpassword = (formData) => {
+  console.log('重置数据:', formData);
+};
+
+const switchToLogin = () => {
+  currentForm.value = 'login';
+};
+</script>
+
+<style scoped>
+.masterresetpassword-page {
+  display: flex;
+  width: 75%;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
