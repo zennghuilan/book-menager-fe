952e192f286fa21a5875b1a28f0bc0eca28820a8
曾慧兰 <234808961@qq.com>
2025-11-05 14:19:54 +0800

    完成了读者注册 登录的页面

---
+++ src/views/ReaderRegister-page.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="readerregister-page">
+    <ReaderLogin v-if="currentForm === 'login'" />
+    <ReaderRegister
+    v-else
+    :switch-to-login="switchToLogin"
+    :on-submit="handleRegister"
+    />
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ReaderLogin from '@/components/ReaderLogin.vue';
+import ReaderRegister from '@/components/ReaderRegister.vue';
+
+const currentForm = ref('register'); // 默认显示注册页面
+
+const handleRegister = (formData) => {
+    console.log('注册数据:', formData);
+};
+
+// 从注册切换到登录
+const switchToLogin = () => {
+    currentForm.value = 'login';
+};
+</script>
+
+
+<style scoped>
+.readerregister-page {
+  display: flex;
+  width: 75%;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
