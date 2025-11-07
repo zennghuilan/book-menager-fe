5ab74817f560f8eecf11f7aaef41c1dde0be51f4
曾慧兰 <234808961@qq.com>
2025-11-07 18:31:46 +0800

    将原来的标签页换成了layout布局 美观性极大提高了

--- src/views/MasterOption-page.vue
+++ src/views/MasterOption-page.vue
@@ -1,79 +1,72 @@
 <template>
-  <a-menu
-    v-model:selectedKeys="current"
-    mode="horizontal"
-    :items="items"
-    @click="handleMenuClick"
-  />
-  <router-view/>
+  <a-layout>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          mode="inline"
+          :style="{ height: '100%' }"
+          @click="OptionClick"
+        >
+          <a-sub-menu key="person">
+            <template #title>
+              <span>
+                <user-outlined />
+                个人主页
+              </span>
+            </template>
+            <a-menu-item key="introduction">用户信息</a-menu-item>
+            <a-menu-item key="borrowdata">借阅记录</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="function">
+            <template #title>
+              <span>
+                <laptop-outlined />
+                功能选择
+              </span>
+            </template>
+            <a-menu-item key="borrow">借书</a-menu-item>
+            <a-menu-item key="return">还书</a-menu-item>
+            <a-menu-item key="sreach">查询</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="return">
+            <template #title>
+              <span>
+                <notification-outlined />
+                退出
+              </span>
+            </template>
+            <a-menu-item key="auth">退出登录</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout style="padding: 24px">
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+          Content
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script setup>
-import { h, ref } from 'vue';
-import { useRouter } from 'vue-router';
-const router = useRouter();
-const current = ref(['mail']);
-const items = ref([
-  {
-    key: 'check',
-    label: '审核申请',
-    title: '审核申请',
-  },
-  {
-    key: 'manage',
-    label: '用户信息管理',
-    title: '用户信息管理',
-  },
-  {
-    key: 'functions',
-    label: '书籍改动',
-    title: '书籍改动',
-    children: [
-      {
-        type: 'group',
-        label: '图书处理',
-        children: [
-          {
-            label: '添加',
-            key: 'add',
-          },
-          {
-            label: '减少',
-            key: 'reduce',
-          },
-        ],
-      },
-    ],
-  },
-  {
-  key: 'exit',
-  label: h(
-      'a',
-      {
-        href: '/option',
-        target: '_blank',
-      },
-      '退出登录',
-    ),
-    title: '退出登录',
-  },
-]);
+import { ref } from 'vue';
+import router from '@/router';
 
-const handleMenuClick = ({ key }) => {
-  if (key === 'check') {
-    router.push('/check');
-  }
-  if (key === 'manage') {
-    router.push('/manage');
-  }
-  if (key === 'add') {
-    router.push('/add');
-  }
-  if (key === 'reduce') {
-    router.push('/reduce');
-  }
-  if (key === 'exit') {
-    router.push('/auth');
-  }
+const selectedKeys2 = ref(['1']);
+const openKeys = ref(['return']);
+const OptionClick = ({ key }) => {
+  router.push(`/${key}`);
 };
 </script>
+
+<style scoped>
+.site-layout-background {
+  background: #fff;
+  width: 100%;
+  height: 100%;
+}
+</style>
