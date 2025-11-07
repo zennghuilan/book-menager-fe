65c22c2b02c7db14b5ccf894fbfc8f8081b98efb
曾慧兰 <234808961@qq.com>
2025-11-07 20:56:58 +0800

    路由跳转基本全部完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,14 @@
 // router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
-import OptionPage from '@/views/Option-page.vue'
-import BorrowBook from '@/components/BorrowBook.vue';
-import ReturnBook from '@/components/ReturnBook.vue';
-import SearchPage from '@/views/Search-page.vue';
+
 import AuthPage from '@/views/Auth-page(Login).vue'
 import AuthPageMasterLogin from '@/views/Auth-page(masterLogin).vue';
 import ReaderRegisterPage from '@/views/ReaderRegister-page.vue';
 import ReaderResetpasswordPage from '@/views/ReaderResetpassword-page.vue';
 import MasterRegister from '@/components/MasterRegister.vue';
 import MasterResetPassword from '@/views/MasterResetpassword-page.vue'
-import Addbook from '@/components/AddBook.vue';
-import ReduceBook from '@/components/ReduceBook.vue';
-import ReaderHome from '@/views/ReaderHome.vue';
+import ReaderOption from '@/views/ReaderOption.vue';
+import MasterOption from '@/views/MasterOption.vue';
 
 const routes = [
   {
@@ -35,7 +31,7 @@ const routes = [
     component: ReaderRegisterPage//读者的注册页面
   },
   {
-    path: '/readerreset-password',
+    path: '/readerresetpassword',
     name: 'ResetPassword',
     component: ReaderResetpasswordPage//读者的重置密码页面
   },
@@ -50,40 +46,14 @@ const routes = [
     component: MasterResetPassword//图书管理员的重置密码页面
   },
   {
-    path: '/option',
-    name: 'Option',
-    component: OptionPage//在成功登陆之后读者对用户的功能的选择界面 虽然对美观还是不满意；
-  },
-    {
-    path: '/borrow',
-    name: 'BorrowBook',
-    component: BorrowBook//读者的借书
+    path: '/readeroption',
+    name: 'Readeroption',
+    component: ReaderOption//读者的个人主页 查看借阅的书籍信息 和 个人信息
   },
   {
-    path: '/return',
-    name: 'Return',
-    component: ReturnBook//读者的还书
-  },
-
-  {
-    path: '/search',
-    name: 'Search',
-    component: SearchPage//搜索的页面 要实现在搜索后就可以拿到对应的图书的信息卡片
-  },
-  {
-    path: '/add',
-    name: 'Add',
-    component: Addbook//图书管理员的书籍添加
-  },
-  {
-    path: '/reduce',
-    name: 'Reduce',
-    component: ReduceBook//图书管理员的书籍减少
-  },
-    {
-    path: '/readerhome',
-    name: 'Readerhome',
-    component: ReaderHome//读者的个人主页 查看借阅的书籍信息 和 个人信息
+    path: '/masteroption',
+    name: 'Masteroption',
+    component: MasterOption//读者的个人主页 查看借阅的书籍信息 和 个人信息
   },
 ];
 
