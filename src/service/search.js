29ad4bd5f1732b19a4fa23f64619688f2143b89f
曾慧兰 <234808961@qq.com>
2025-11-09 19:48:50 +0800

    完成了搜索的发请求

--- src/service/search.js
+++ src/service/search.js
@@ -1,16 +1,19 @@
 import axios from "axios"
 
-const search = async (searchValue, token) => {
+const search = async (searchValue, token) => {//用这个异步函数来接收搜索值和 token 作为参数。
   try {
-    const response = await axios.post(
-      "127.0.0.1:8080/api/books/search",
+    const query = new URLSearchParams({//
+      searchValue:searchValue,
+    })
+    const url = `${import.meta.env.VITE_SERVER_URL}/introduce?${query.toString()}`
+    // get无请求体
+    const response = await axios.get(
+      url,//后端的接口
       {
-        searchValue: searchValue
-      },
-      {
-        headers : {
-          Authorization: `Bearer ${token}`
-        }
+        headers : {//请求头
+          Authorization: `Bearer ${token}`//认证的信息，也就是授权 从后端拿到密钥
+        },
+        withCredentials: true   // 带上cookies，用于csrf认证
       }
     );
 
@@ -19,7 +22,7 @@ const search = async (searchValue, token) => {
     }
   }
   catch (e) {
-    console.log(`发生异常${e}`)
+    console.log(`发生异常: ${e}`)
     return null;
   }
 }
