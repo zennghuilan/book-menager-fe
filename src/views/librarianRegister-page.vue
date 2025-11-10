98f1c2f5f56cc83b7c8585bd08511ffaca052585
曾慧兰 <234808961@qq.com>
2025-11-10 19:35:55 +0800

    图书管理员的注册页面

---
+++ src/views/librarianRegister-page.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="masterregister-page">
+    <MasterLogin v-if="currentForm === 'login'" />
+    <MasterRegister v-else :switch-to-login="switchToLogin" :on-submit="handleRegister" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import MasterLogin from '@/views/Auth-page(librarianLogin).vue'
+import MasterRegister from '@/components/MasterRegister.vue'
+
+const currentForm = ref('register') // 默认注册页面
+
+const handleRegister = (formData) => {
+  console.log('注册数据:', formData)
+}
+
+// 从注册切换到登录
+const switchToLogin = () => {
+  currentForm.value = 'login'
+}
+</script>
+
+<style scoped>
+.masterresetpassword-page {
+  display: flex;
+  width: 75%;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
