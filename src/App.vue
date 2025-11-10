860387fc803408a7796a2a2846c3d78a0d74b2dd
曾慧兰 <234808961@qq.com>
2025-11-10 19:56:32 +0800

    app 暂定

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
 <script>
@@ -16,7 +14,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  height: 100vh;
+  height: 100%;
   background-color: #f0f2f5;
   display: flex;
   justify-content: center;
