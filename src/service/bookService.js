fcb5c066b68ca48fc3b78c1098e93ebc64a06689
曾慧兰 <234808961@qq.com>
2025-11-10 15:59:40 +0800

    完成了对读者对借书还书的功能的请求

---
+++ src/service/bookService.js
@@ -0,0 +1,50 @@
+// 这里是读者
+import axios from "axios"
+
+export const borrowBook = async (bookId, token) => {//将该函数导出，供其他模块使用
+  try {
+    const response = await axios.post(
+          "/api/user/borrow_book/",
+      {
+        book_id: bookId
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',//请求体是 JSON 格式
+          'Authorization': `Bearer ${token}`//用户身份验证和授权
+        }
+      }
+    );
+
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (e) {
+    console.log(`借书发生异常: ${e}`);
+    throw e; //在组件中处理
+  }
+}
+
+export const returnBook = async (bookId, token) => {
+  try {
+      const response = await axios.post(
+          "/api/user/return_book/",
+      {
+        book_id: bookId
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    );
+
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (e) {
+    console.log(`还书发生异常: ${e}`);
+    throw e; //在组件中处理
+  }
+}
